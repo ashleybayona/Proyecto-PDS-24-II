55d5f20ae6a7588fcf9fff6a9a846f9c9110fd93
achli <ashley.bayonav@gmail.com>
2024-09-24 13:13:04 -0500

    cambio fake

--- Base de datos/SQLQuery1.sql
+++ Base de datos/SQLQuery1.sql
@@ -1,24 +1,24 @@
 create database Proyecto1
 use Proyecto1
-create table Productos2 (
+create table Productos (
     ProductoID int primary key identity(1,1),
     Nombre nvarchar(100) NOT NULL,
     Precio decimal(10, 2) NOT NULL
 );
 
-create table Ordenes2 (
+create table Ordenes (
     OrdenID int primary key identity(1,1),
     FechaOrden  datetime default GETDATE(),
     Total decimal(10, 2) not null
 );
 
-create table OrdenesProductos2 (
+create table OrdenesProductos (
     OrdenProductoID int primary key identity(1,1),
-    OrdenID int foreign key references Ordenes2(OrdenID),
-    ProductoID int foreign key references Productos2(ProductoID),
+    OrdenID int foreign key references Ordenes(OrdenID),
+    ProductoID int foreign key references Productos(ProductoID),
     Cantidad int not null
 );
-insert into  Productos2 (Nombre, Precio)
+insert into  Productos (Nombre, Precio)
 values 
 ('Producto A', 10.50),
 ('Producto B', 20.00),
