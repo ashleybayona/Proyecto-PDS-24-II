9ec1e78d9fd01931abb21af7aa2da5762e3caa75
elesel-01 <jerson.valqui@unmsm.edu.pe>
2024-09-24 13:11:27 -0500

    a

--- Base de datos/SQLQuery1.sql
+++ Base de datos/SQLQuery1.sql
@@ -1,24 +1,24 @@
 create database Proyecto1
 use Proyecto1
-create table Productos2 (
+create table Productos20 (
     ProductoID int primary key identity(1,1),
     Nombre nvarchar(100) NOT NULL,
     Precio decimal(10, 2) NOT NULL
 );
 
-create table Ordenes2 (
+create table Ordenes20 (
     OrdenID int primary key identity(1,1),
     FechaOrden  datetime default GETDATE(),
     Total decimal(10, 2) not null
 );
 
-create table OrdenesProductos2 (
+create table OrdenesProductos20 (
     OrdenProductoID int primary key identity(1,1),
-    OrdenID int foreign key references Ordenes2(OrdenID),
-    ProductoID int foreign key references Productos2(ProductoID),
+    OrdenID int foreign key references Ordenes20(OrdenID),
+    ProductoID int foreign key references Productos20(ProductoID),
     Cantidad int not null
 );
-insert into  Productos2 (Nombre, Precio)
+insert into  Productos20 (Nombre, Precio)
 values 
 ('Producto A', 10.50),
 ('Producto B', 20.00),
