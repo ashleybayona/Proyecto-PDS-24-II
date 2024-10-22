00cba3ea3493eab4d7b90c6ce9d95608f462f680
NelsonRamos1512 <nelson.ramosa@unmsm.edu.pe>
2024-10-22 01:12:35 -0500

    "ubi"

--- mainsql.py
+++ mainsql.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from env.Lib.fastapi import FastAPI, HTTPException
 from env.Lib.werkzeug.security import generate_password_hash, check_password_hash
 from scheme import *
 from conexion_sqlserver import * #importa el archivo de la conexión a la bd
