9a82a8142c7f3ea3a5d952e7bdeda4376267dcdb
achli <ashley.bayonav@gmail.com>
2024-10-24 21:56:55 -0500

    aeiou

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from config.connect_gcloud_mysql import *
 from scheme import *
 from typing import List
+import uvicorn
 
 app = FastAPI()
 
 app.title = 'API La Puntita' #titulo para la documentación dentro de fastapi
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Usa 8000 como puerto por defecto si no se encuentra la variable
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 '''
 http://127.0.0.1:8000
