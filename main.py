232680d2b1acf34447095d3ce50449db299ce4cd
achli <ashley.bayonav@gmail.com>
2024-10-31 12:26:59 -0500

    mejor manejo de archivos, routers

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-from fastapi import FastAPI, HTTPException, Depends
+#LIBRERIAS
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from werkzeug.security import generate_password_hash, check_password_hash 
-from config.connect_gcloud_mysql import *
-from datetime import timedelta
-from scheme import *
-from typing import List
 import uvicorn
-import asyncio 
+import os
+
+#ARCHIVOS
+from routers.users import users_router
+from routers.products import products_router
+from routers.payment import payment_router
+from routers.sales import sales_router
+from routers.sale_detail import sale_detail_router
+from routers.login import login_router
 
 app = FastAPI()
+
+app.include_router(users_router)
+app.include_router(products_router)
+app.include_router(payment_router)
+app.include_router(sales_router)
+app.include_router(sale_detail_router)
+app.include_router(login_router)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
-    allow_methods=['*']
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.title = 'API La Puntita' 
@@ -22,198 +34,6 @@ app.title = 'API La Puntita'
 @app.get("/", tags=['Home'])
 def home():
     return "wasaaaa"
-
-#PARA VER TODOS LOS PRODUCTOS
-@app.get("/productos", tags=['Producto'], response_model=List[Producto]) #funciona
-def get_productos():
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor(dictionary=True) as cursor:
-            cursor.execute('select * from producto')
-            productos = cursor.fetchall() 
-        return productos
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#PARA VER TODOS LOS USUARIOS
-@app.get("/usuarios", tags=['Usuario'],response_model=List[Usuario]) #funciona
-def get_usuarios():
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor(dictionary=True) as cursor:
-            cursor.execute("select * from usuario")
-            usuarios = cursor.fetchall()
-        return usuarios
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#PARA VER TODAS LAS VENTAS
-@app.get("/ventas", tags=['Venta'], response_model=List[Venta]) #funciona
-def get_ventas():
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor(dictionary=True) as cursor:
-            cursor.execute("select * from venta")
-            ventas = cursor.fetchall()
-        return ventas
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#PARA CREAR USUARIO
-@app.post("/usuario", tags=['Usuario']) #funciona
-def create_usuario(data: CreateUser):
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor() as cursor:
-            new_user = data.dict()
-            valid_email = asyncio.run(validar_email(new_user['email']))#new_user['email']
-            if valid_email:
-                new_user["passw"] = generate_password_hash(data.passw, 'pbkdf2:sha256:30', 30)
-                cursor.callproc('crearusuario', [new_user['dni'], new_user['nombre'], new_user['apellido'], new_user['telefono'], 
-                                                new_user['email'], new_user['direccion'], new_user['referencia'], new_user['passw']])
-            else:
-                raise HTTPException(status_code=400, detail="El correo electrónico no es válido.")
-            conexion.commit()
-        return {"message": "Usuario creado exitosamente"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#PARA ACTUALIZAR USUARIO
-@app.put("/user/{id_user}", tags=['Usuario'], response_model=Usuario) #funciona
-def update_user(data_update: UpdateUser, id_user: int):
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor(dictionary=True) as cursor:
-            if data_update.passw:
-                passw = generate_password_hash(data_update.passw, 'pbkdf2:sha256:30', 30)
-            else:
-                passw = data_update.passw
-            cursor.callproc('updateusuario', [id_user, data_update.telefono, data_update.email, data_update.direccion, data_update.referencia, passw])
-            conexion.commit()
-            cursor.execute('select * from usuario where idUsuario = %s', (id_user,)) #ya funciona ji
-            result = cursor.fetchone()
-            return result
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#PARA ELIMINAR USUARIO
-@app.delete("/user/{id_user}", tags=['Usuario']) #funciona
-def delete_user(id_user: int):
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor() as cursor:
-            cursor.callproc('deleteusuario', [id_user])
-            conexion.commit()
-        return {"message": "Usuario eliminado correctamente"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#PARA CREAR VENTA
-@app.post("/ventas", tags=['Venta']) #funciona
-def create_venta(iduser: int):
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor() as cursor:
-            print('aka')
-            cursor.callproc('crearventa', [iduser]) 
-            print('aka2')
-            for resultado in cursor.stored_results():
-                id_venta = resultado.fetchone()[0] 
-            conexion.commit()
-        return {"message": "Venta creada exitosamente", "IdVenta": id_venta}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#PARA AGREGAR PRODUCTOS AL DETALLE VENTA
-@app.post("/detalle-venta", tags=['DetalleVenta']) #funciona
-def add_detalle_venta(detventa: DetalleVenta):
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor() as cursor:
-            cursor.callproc('agregarproductodetalleventa', [detventa.idVenta, detventa.idProducto, detventa.cantidad])
-            conexion.commit()
-        return {"message": "Producto agregado a detalle de venta exitosamente"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#PARA ACTUALIZAR LOS IMPORTES EN LA TABLA VENTA
-@app.put("/venta/{id_venta}/actualizar-importes", tags=['Venta']) #funciona
-def update_importes_venta(id_venta: int):
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor() as cursor:
-            cursor.callproc('actualizarimportesventa', [id_venta])
-            conexion.commit()
-        return {"message": "Importes de la venta actualizados exitosamente"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
-
-#TOKEN PARA VERIFICAR CORREO
-#...
-
-oauth2_scheme = OAuth2PasswordBearer("/token")
-
-#LOGIN DE USUARIOS
-@app.get("/users/me", tags=['Usuario']) #ESTE FALTA COMPLETAR
-def read_users(token: str = Depends(oauth2_scheme)):
-    return 'USER'
-
-@app.post("/token", tags=['Usuario']) #FALTA MEJORAR
-def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    try:
-        conexion = conexion_pool.get_connection()
-        with conexion.cursor(dictionary=True) as cursor:
-            cursor.execute('select * from usuario where email = %s and eliminado = 0', (form_data.username,))
-            user = cursor.fetchone()
-            if user:
-                check_passw = check_password_hash(user['passw'], form_data.password)
-                if check_passw:
-                    '''acces_token_expires = timedelta(minutes=30)
-                    acces_token_jwt = create_token()'''
-                    return {
-                        'message': 'Usuario logueado exitosamente',
-                        'user_id': user['idUsuario']
-                    }
-                    '''{ #aka falla
-                        "access_token": "gaaa",
-                        "token_type": "bearer"
-                    }'''
-                else:
-                    raise HTTPException(status_code=400, detail="Contraseña incorrecta")
-            else:
-                raise HTTPException(status_code=400, detail="Usuario no encontrado")           
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    finally:
-        if conexion.is_connected():
-            conexion.close()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
