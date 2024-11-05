d88c0e3cda2296a1c6c49aa1c67273ad87eff8d5
achli <ashley.bayonav@gmail.com>
2024-11-05 18:55:13 -0500

    prueba del ep create-order

--- routers/payment.py
+++ routers/payment.py
@@ -25,12 +25,44 @@ client = PayPalHttpClient(environment)
 '''@payment_router.post('/create-order/{idventa}')
 async def create_order_paypal(idventa: int, request: Request):
     try:
-        #data = await request.json()
-        #print(data)
+        data = await request.json() #items de la compra -> ESTO TMB SE PUEDE HACER CON PROCEDIMIENTO ALMACENADO
         total = get_total(idventa)
+        value = f"{total:.2f}" 
+
         request = OrdersCreateRequest()
 
         request.prefer('return=representation')
+        request.request_body(
+            {
+                'intent': 'CAPTURE',
+                'purchase_units': [
+                    {
+                        'amount': {
+                            'currency_code': 'USD', #ESTO DEBE DE SER CAMBIADO A PEN, PERO NO SE PUEDE POR EL MOMENTO
+                            'value': value
+                        }
+                    }
+                ],
+                'items': data
+            }
+        )
+        response = client.execute(request)
+        #FALTA AGREGAR Q EL ID SE GUARDE EN LA BD
+        return JSONResponse(content={id: response.result.id })
+    except IOError:
+        print(IOError)'''
+
+
+#PRUEBA DEL ENDPOINT SIN REQUEST DEL FRONT
+@payment_router.post('/create-order/{idventa}')
+def create_order_paypal(idventa: int):
+    try:
+        total = get_total(idventa)
+        value = f"{total:.2f}"  # Convierte total a string con dos decimales
+        request = OrdersCreateRequest()
+
+        request.prefer('return=representation')
+        print('antes d request body')
         request.request_body(
             {
                 'intent': 'CAPTURE',
@@ -38,40 +70,17 @@ async def create_order_paypal(idventa: int, request: Request):
                     {
                         'amount': {
                             'currency_code': 'USD',
-                            'value': f'{total}'
+                            'value': value
                         }
                     }
                 ],
                 #'items': data
             }
-        )
+        ) 
+        print(request)
         response = client.execute(request)
-        return JSONResponse(content={id: response.result.id })
-    except IOError:
-        print(IOError)
-'''
-@payment_router.post('/create-order/{idventa}')
-async def create_order_paypal(idventa: int):
-    try:
-        total = get_total(idventa)
-        request = OrdersCreateRequest()
-
-        request.prefer('return=representation')
-        request.request_body(
-            {
-                'intent': 'CAPTURE',
-                'purchase_units': [
-                    {
-                        'amount': {
-                            'currency_code': 'PEN',
-                            'value': f'{total}'
-                        }
-                    }
-                ],
-                #'items': data
-            }
-        )
-        response = client.execute(request)
-        return {'id': response.result.id, 'total': total}, 
-    except IOError:
-        print(IOError)
+        print('despues de response')
+        return JSONResponse(content={'id': response.result.id })
+    except Exception as e:
+        print(e)
+        return JSONResponse(content={'error': 'Error al crear el pedido'})
