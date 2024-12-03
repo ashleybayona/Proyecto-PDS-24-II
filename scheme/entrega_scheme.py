0d2648e9bff8fb1484c1d84988de6bb6701dc869
achli <ashley.bayonav@gmail.com>
2024-12-03 02:52:43 -0500

    cambio timedelta2

--- scheme/entrega_scheme.py
+++ scheme/entrega_scheme.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from datetime import datetime, timedelta
 
 #ARCHIVOS
@@ -10,6 +10,15 @@ class Entrega(BaseModel):
     #idRepartidor: int
     #fechaEntrega: datetime
     estadoEntrega: EstadoEntrega
-    horaEstimada: timedelta
+    horaEstimada: str
     nombreRepartidor: str
     telefRepartidor: str
+
+    @validator("horaEstimada", pre=True)
+    def convertir_timedelta(cls, value):
+        if isinstance(value, timedelta):
+            # Convierte timedelta a formato HH:MM:SS
+            horas, resto = divmod(value.seconds, 3600)
+            minutos, segundos = divmod(resto, 60)
+            return f"{horas:02}:{minutos:02}:{segundos:02}"
+        return value
